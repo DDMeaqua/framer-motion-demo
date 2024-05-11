ceb09daae5aaf7cfcbcfe16d1ad70645b3e59808
DDMeaqua <lzlloveyou@vip.qq.com>
2024-05-11 18:50:47 +0800

    first commit

--- src/content.tsx
+++ src/content.tsx
@@ -1,10 +1,12 @@
 import cssText from "data-text:~style.css"
+import { motion } from "framer-motion"
 import type { PlasmoCSConfig } from "plasmo"
-
-import { CountButton } from "~features/count-button"
+import { useState } from "react"
+import Icon from "src/features/icon"
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://www.plasmo.com/*"]
+  matches: ["<all_urls>"],
+  all_frames: false
 }
 
 export const getStyle = () => {
@@ -13,11 +15,64 @@ export const getStyle = () => {
   return style
 }
 
+const variants = {
+  expanded: { height: "auto" },
+  collapsed: { height: 2.5 + "rem" }
+}
+
 const PlasmoOverlay = () => {
+  const [hovered, setHovered] = useState(false)
+  const btnCss = "p-2 rounded-md hover:bg-blue-500 hover:text-white"
+
   return (
-    <div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8">
-      <CountButton />
-    </div>
+    <motion.div
+      layout={true}
+      initial="collapsed"
+      animate={hovered ? "expanded" : "collapsed"}
+      variants={variants}
+      transition={{ duration: 0.3, ease: "linear" }}
+      className={`z-50 flex fixed top-32 right-3 w-12 transition-all text-black shadow bg-gray-100`}
+      style={{
+        borderRadius: "8px"
+      }}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}>
+      {hovered ? (
+        <div className="flex flex-col justify-between items-center w-full h-full overflow-hidden p-2">
+          <button className={btnCss}>
+            <Icon />
+          </button>
+          <button className={btnCss}>
+            <Icon />
+          </button>
+          <button className={btnCss}>
+            <Icon />
+          </button>
+          <div className="border border-b-1 border-gray-400 w-full my-1"></div>
+          <button className={btnCss}>
+            <Icon />
+          </button>
+        </div>
+      ) : (
+        <div className="w-full h-full flex justify-center items-center shadow rounded-lg my-auto">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24">
+            <g
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2">
+              <rect width="18" height="14" x="3" y="8" rx="2" />
+              <path d="M12 5a3 3 0 1 0-3 3m6 0a3 3 0 1 0-3-3m0 0v17m9-7H3" />
+            </g>
+          </svg>
+        </div>
+      )}
+    </motion.div>
   )
 }
 
--- src/features/count-button.tsx
+++ src/features/count-button.tsx
@@ -1,18 +1,14 @@
-import { useReducer } from "react"
 
 export const CountButton = () => {
-  const [count, increase] = useReducer((c) => c + 1, 0)
 
   return (
     <button
-      onClick={() => increase()}
       type="button"
-      className="plasmo-flex plasmo-flex-row plasmo-items-center plasmo-px-4 plasmo-py-2 plasmo-text-sm plasmo-rounded-lg plasmo-transition-all plasmo-border-none
-      plasmo-shadow-lg hover:plasmo-shadow-md
-      active:plasmo-scale-105 plasmo-bg-slate-50 hover:plasmo-bg-slate-100 plasmo-text-slate-800 hover:plasmo-text-slate-900">
-      Count:
-      <span className="plasmo-inline-flex plasmo-items-center plasmo-justify-center plasmo-w-8 plasmo-h-4 plasmo-ml-2 plasmo-text-xs plasmo-font-semibold plasmo-rounded-full">
-        {count}
+      className="flex flex-row items-center p-2 text-sm rounded-lg transition-all border-none
+      shadow-lg hover:shadow-md
+      active:scale-105 bg-slate-50 hover:bg-slate-100 text-slate-800 hover:text-slate-900">
+      <span className="inline-flex items-center justify-center text-xs font-semibold rounded-full w-4 h-4">
+        <img src="https://www.hehehai.cn/_next/image?url=%2Fstatic%2Fimages%2Femojis%2Ffind.png&w=1920&q=75" alt="logo" />
       </span>
     </button>
   )
